764209c5c069f1e153c2215e057fe33b996ba7c2
mmuravytskyi <purchasedinukrain@gmail.com>
2020-11-22 11:57:49 +0100

    Analise class completed

--- core/actorClasses/imageAnalyse.py
+++ core/actorClasses/imageAnalyse.py
@@ -12,6 +12,44 @@ PATH_TO_RUN_TIME = "./runtime-data"
 COUNTRY = "eu"
 REGION = "pl"
 
+
+def singleton(class_):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return getinstance
+
+
+@singleton
+class MLInstance:
+    # logger for library-related messages
+    log = logging.getLogger(__name__)
+    ch = logging.StreamHandler(stream=sys.stdout)
+    if ['debug'] == 1:
+        print('elo')
+    log.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s',
+                                  datefmt='%H:%M:%S')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+    recognize_alg = Alpr(COUNTRY, PATH_TO_CONF, PATH_TO_RUN_TIME)
+
+    # verify library's availability
+    if not recognize_alg.is_loaded():
+        log.error("Error loading a library")
+    else:
+        log.info("Starting detection process...")
+
+    recognize_alg.set_top_n(1)
+    recognize_alg.set_country(COUNTRY)
+    recognize_alg.set_default_region(REGION)
+
+
 class ImageAnalyseInt(ABC):
     @staticmethod
     @abstractmethod
@@ -21,41 +59,20 @@ class ImageAnalyseInt(ABC):
 
 class ImageAnalyse(ImageAnalyseInt):
     @staticmethod
-    def analyse(_id, frame, queue):
-        # logger for library-related messages
-        log = logging.getLogger(__name__)
-        ch = logging.StreamHandler(stream=sys.stdout)
-        if ['debug'] == 1:
-            print('elo')
-        log.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s',
-                                      datefmt='%H:%M:%S')
-        ch.setFormatter(formatter)
-        log.addHandler(ch)
-
-        recognize_alg = Alpr(COUNTRY, PATH_TO_CONF, PATH_TO_RUN_TIME)
-
-        # verify library's availability
-        if not recognize_alg.is_loaded():
-            log.error("Error loading a library")
-        else:
-            log.info("Starting detection process...")
-
-        recognize_alg.set_top_n(1)
-        recognize_alg.set_country(COUNTRY)
-        recognize_alg.set_default_region(REGION)
+    def analyse(_id, frame, ml_instance) -> []:
 
         # model requires bytes array, so read image in binary mode
         # TODO define frame
         # jpeg_bytes = open(frame, "rb").read()
         jpeg_bytes = bytes(frame)
-        results = recognize_alg.recognize_array(jpeg_bytes)
+        results = ml_instance.recognize_alg.recognize_array(jpeg_bytes)
 
-        plate = None
+        plates = []
         for regions in results['results']:
             plate = LicensePlate.LicensePlate(str(regions['plate']), float(regions['confidence']),
-                                              dict(regions['coordinates']),  float(regions['processing_time_ms']))
-            queue.put(plate)
+                                              dict(regions['coordinates']), float(regions['processing_time_ms']))
+            plates.append(plate)
+        return plates
 
 
 class ImageAnalyseMock(ImageAnalyseInt):
@@ -65,4 +82,3 @@ class ImageAnalyseMock(ImageAnalyseInt):
         queue.put(frame)
         queue.task_done()
         return frame
-
