ca1cedcff0661a24c29c7963d1157cdaa9591f62
mmuravytskyi <purchasedinukrain@gmail.com>
2020-11-29 17:12:14 +0100

    compiled libraries & documentation added

--- core/actorClasses/imageAnalyse.py
+++ core/actorClasses/imageAnalyse.py
@@ -1,15 +1,17 @@
 import random
 import time
-import pprint
 import logging
 import sys
 from abc import abstractmethod, ABC
-from openalpr import Alpr
-from argparse import ArgumentParser
 from core.dataClasses import LicensePlate
+from pathlib import Path
 
-PATH_TO_CONF = "./openalpr/openalpr.myconfig.conf"
-PATH_TO_RUN_TIME = "./runtime-data"
+# module files are inside another directory, so python search dirs should be expanded
+sys.path.append("alpr")
+from lprec import Alpr
+
+PATH_TO_CONF = Path("alpr/config.alpr.conf")
+PATH_TO_RUN_TIME = Path("alpr/runtime-data")
 COUNTRY = "eu"
 REGION = "pl"
 from core.dataClasses.LicensePlate import LicensePlate
@@ -30,41 +32,56 @@ def singleton(class_):
 
 @singleton
 class MLInstance:
-    # logger for library-related messages
-    log = logging.getLogger(__name__)
-    ch = logging.StreamHandler(stream=sys.stdout)
-    if ['debug'] == 1:
-        print('elo')
-    log.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s',
-                                  datefmt='%H:%M:%S')
-    ch.setFormatter(formatter)
-    log.addHandler(ch)
+    """
+    In order to operate only on one ML instance, thus saving memory in
+    multi-threading implementation, singleton class is provided. This way
+    all the instances of analyse function will operate on the same ML instance.
 
-    recognize_alg = Alpr(COUNTRY, PATH_TO_CONF, PATH_TO_RUN_TIME)
+    All created instances of a singleton class will be pointing on the same object.
+    """
+    def __init__(self):
+        # logger for library-related messages
+        log = logging.getLogger(__name__)
+        ch = logging.StreamHandler(stream=sys.stdout)
+        if ['debug'] == 1:
+            print('elo')
+        log.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s',
+                                      datefmt='%H:%M:%S')
+        ch.setFormatter(formatter)
+        log.addHandler(ch)
 
-    # verify library's availability
-    if not recognize_alg.is_loaded():
-        log.error("Error loading a library")
-    else:
-        log.info("Starting detection process...")
+        recognize_alg = Alpr(COUNTRY, PATH_TO_CONF, PATH_TO_RUN_TIME)
 
-    recognize_alg.set_top_n(1)
-    recognize_alg.set_country(COUNTRY)
-    recognize_alg.set_default_region(REGION)
+        # verify library's availability
+        if not recognize_alg.is_loaded():
+            log.error("Error loading a library")
+        else:
+            log.info("Starting detection process...")
+
+        # TODO: clean up unnecessary parameters, given by default
+        recognize_alg.set_top_n(1)
+        recognize_alg.set_country(COUNTRY)
+        recognize_alg.set_default_region(REGION)
 
 
 class ImageAnalyseInt(ABC):
     @staticmethod
     @abstractmethod
-    def analyse(_id, frame, queue):
+    def analyse(_id, frame) -> []:
         pass
 
 
 class ImageAnalyse(ImageAnalyseInt):
     @staticmethod
-    def analyse(_id, frame, ml_instance) -> []:
-
+    def analyse(_id, frame) -> []:
+        """
+        Analyse function uses pre-initialized ML instance to analyse an image.
+        :param _id: unique process identifier.
+        :param frame: dataclass object containing a frame for the analysis.
+        :return: a list of LicensePlate objects referring to the found plates.
+        """
+        ml_instance = MLInstance()
         # model requires bytes array, so read image in binary mode
         # TODO define frame
         # jpeg_bytes = open(frame, "rb").read()
