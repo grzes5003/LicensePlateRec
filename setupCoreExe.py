7aab1e056075f21da7df706e6b7955901f278dec
grzes5003 <grzes.kulinski@gmail.com>
2020-12-07 16:51:48 +0100

    Minor changes

--- setupCoreExe.py
+++ setupCoreExe.py
@@ -7,7 +7,7 @@ options = {
 include_files = ['openalpr_x86', 'config.toml']
 
 setup(name="test02",
-      version="0.1",
-      description="My GUI application!",
+      version="0.2",
+      description="LicensePlateRec!",
       options={'build_exe': {'include_files': include_files}},
       executables=[Executable("mainCore.py")])
